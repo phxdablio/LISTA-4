80b5dc12602bb69544c18027f51d2c8ebabb5e89
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2022-11-16 16:21:22 -0300

    Add files via upload

---
+++ QUESTÃO 06 (LISTA4).cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+
+int main (){
+
+int i;
+
+
+for (i = 10; i >=0; i = i-1)
+{
+	printf ("%d ",i);
+}
+printf ("FIM !");
+}
+
+
