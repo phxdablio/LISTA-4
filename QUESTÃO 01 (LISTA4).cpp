dddb2dada57dd9e467ea8c6de4505f1193b88239
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2022-11-16 12:34:13 -0300

    Add files via upload

---
+++ QUESTÃO 01 (LISTA4).cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+
+int main (){
+
+int numero,i;
+
+printf("[DIGITE UM NUMERO DESEJADO]\n");
+scanf ("%i",&numero);
+
+for (i = 1; i <= numero; i = i +1)
+{
+	printf ("%d\n",i);
+}
+
+
+
+}
