302d050a1e430ce9c6785ca9150c54f9b9dae8ac
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2022-11-16 16:36:32 -0300

    Add files via upload

---
+++ QUESTÃO 07 (LISTA4).cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+
+int main (){
+
+int v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,resultado;
+
+printf ("[DIGITE 10 VALORES DESEJADOS]\n");
+scanf ("%d%d%d%d%d%d%d%d%d%d",&v1,&v2,&v3,&v4,&v5,&v6,&v7,&v8,&v9,&v10);
+resultado = v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10;
+
+printf ("RESULTADO E %d",resultado);
+}
+ 
